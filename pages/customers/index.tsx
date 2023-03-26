5740b682175d12724a8975d99dd601d8ce74beb0
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-03-26 10:54:58 +0700

    Static Site Generation with getStaticProps

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,7 +1,48 @@
-import { NextPage } from 'next';
+import { NextPage, GetStaticProps, InferGetServerSidePropsType } from 'next';
 
-const Customers: NextPage = () => {
-   return <h1>Customers</h1>;
+type Customer = {
+   id: number;
+   name: string;
+   industry: string;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+   return {
+      props: {
+         customers: [
+            {
+               id: 1,
+               name: 'John Smith',
+               industry: 'Restaurant',
+            },
+            {
+               id: 2,
+               name: 'Sal Brown',
+               industry: 'Tech',
+            },
+         ] as Customer[],
+      },
+   };
+};
+
+const Customers: NextPage = ({
+   customers,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => {
+   console.log(customers);
+   return (
+      <>
+         <h1>Customers</h1>
+         {customers.map((customer: Customer) => {
+            return (
+               <div>
+                  <p>{customer.name}</p>
+                  <p>{customer.industry}</p>
+                  <p>{customer.id}</p>
+               </div>
+            );
+         })}
+      </>
+   );
 };
 
 export default Customers;
